3754a76436cf5e2bea4100e3395eb7086b0cc9b4
Pranav Kumar <pranavrox.37@gmail.com>
2020-07-13 10:46:30 +0530

    Did not need TeamDesc Adapter

--- app/src/main/java/com/example/footballdb/TeamAdapter.kt
+++ app/src/main/java/com/example/footballdb/TeamAdapter.kt
@@ -1,13 +1,16 @@
 package com.example.footballdb
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.team_info.*
 import kotlin.coroutines.coroutineContext
 
 class TeamAdapter(val teams: ArrayList<TeamProperty>) : RecyclerView.Adapter<TeamAdapter.ViewHolder>() {
@@ -16,16 +19,28 @@ class TeamAdapter(val teams: ArrayList<TeamProperty>) : RecyclerView.Adapter<Tea
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view: View=LayoutInflater.from(parent.context).inflate(R.layout.team_info, parent,false)//sets the view
+
         return ViewHolder(view)
     }
 
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.teaminfo.text= teams[position].strTeam
+        holder.itemView.setOnClickListener(clickListener)
         Glide.with(holder.itemView.context)
             .load(teams[position].strTeamBadge)
             .into(holder.image)
             //league name in text view
+    }
+    private val clickListener: View.OnClickListener = View.OnClickListener { view ->
+        when (view.id) {
+            R.id.team_images -> gotoXScreen()
+        }
+
+    }
+    private fun gotoXScreen() {
+        val intent = Intent(this, Team_Description::class.java)
+        startActivity(intent)
     }
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){//intializing view of card
---
+++ app/src/main/java/com/example/footballdb/Team_Description.kt
@@ -0,0 +1,13 @@
+package com.example.footballdb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.team_info.*
+
+class Team_Description : AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.team_desc)
+    }
+}
